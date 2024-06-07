14895e5b005c999944109d826d8114bbdeb70577
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-06-07 23:06:03 +0100

    task 8

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -3,9 +3,9 @@
 from typing import Callable
 
 
-def make_multiplier(multipler: float) -> Callable[[float], float]:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Return a function that multiplies a float"""
-    def num_func(number: float) -> float:
-        """The function that multiplies a float with a multiple"""
-        return number * multipler
-    return num_func
+    def func(num: float) -> float:
+        """The function that multipies a float with a multiplie"""
+        return num * multiplier
+    return func
