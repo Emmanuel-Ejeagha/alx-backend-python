b9f4edc54d94ab86a06bbab1f733748dfa5ef8d8
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-06-07 22:26:49 +0100

    Added a newline

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-"""This module using python type annotation to add values"
+"""This module using python type annotation to add values"""
 
 
 def concat(str1: str, str2: str) -> str:
