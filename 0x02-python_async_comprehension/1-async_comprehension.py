80d19b8c8f2ef021c76fd60fd6a004aa7610ee8c
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-06-12 00:18:06 +0100

    task 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -9,6 +9,6 @@ async_generator = __import__('0-async_generator').async_generator
 async def async_comprehension() -> List[float]:
     """Iterate through a generator"""
 
-    result = [x async for x in async_generator()]
+    result = [n async for n in async_generator()]
 
     return result
